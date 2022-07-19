bd87090e4dde8f8c405866d39fd4c77ff61327cc
YenyAdama <108185798+YenyAdama@users.noreply.github.com>
2022-07-19 21:26:35 +0200

    lab-avance

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
+console.log("Im ready!");
 // Iteration 1: Names and Input
-//
+let hacker1="yeny";
+let hacker2="chrome"
+console.log("The driver´s name is "+hacker1);
+console.log("The navigator´s name is "+hacker2);
 // Iteration 2: Conditionals
+if(hacker1.length>hacker2.length){
+    let text="The driver has the longest name, it has ";
+    console.log(`${text} ${hacker1.length}` + ` characters.`);
+}else if(hacker1.length==hacker2.length){
+    console.log(`Wow, you both have equally long names, ${hacker1.length}`);
+}else{
+    let text="The navigator´s has the longest name, it has ";
+    console.log(`${text} ${hacker2.length}` + ` characters.`);
+}
+
 
 
 // Iteration 3: Loops
